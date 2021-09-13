34fe3a4dab02bf06323ca9267d05d9dc7e07b3f7
phifour <w.frisch@gmx.at>
2021-09-13 20:23:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from loan import *
+# from loan import *
+#https://xxxcomparelistxxx.azurewebsites.net/lista/a,b,c,d,e,f/listb/a,b,x
+#http://127.0.0.1:5000/lista/a,b,c,d,e,f/listb/a,b,x
 
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/loan/<n>/<r>")
-def loan(n,r):
-    return calc_cashflows(n,r)
-
+def intersect(listx,listy):
+    intersect = []
+    not_in_y = []
+    for elem_a in listx:
+            if elem_a in listy:
+                intersect.append(elem_a)
+            else:
+                not_in_y.append(elem_a)
+    return intersect,not_in_y
 
 @app.route("/lista/<a>/listb/<b>")
 def compare(a,b):
-    # print(a)
     lista = a.split(',')
     listb = b.split(',')
 
-    intersection_b_a = []
-    # intersection_a_b = []
+    intersect_a_b, not_in_y  = intersect(lista,listb)
+    dummy, not_in_x  = intersect(listb,lista)
 
-    for elem_a in lista:
-        if elem_a in listb:
-            intersection_b_a.append(elem_a)
-
-
-    return {'lista':lista,'listb':listb,'intersection_b_a':intersection_b_a}
+    return {'lista':lista,
+    'listb':listb,
+    'intersection_a_b':intersect_a_b,
+    'elements_a_not_in_listb':not_in_y,
+    'elements_b_not_in_lista':not_in_x
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
